8b7306eb459b2ee6ec19ab76ab85ff74bf8bd77d
Pallavi <negipallavi2005@gmail.com>
2025-08-06 22:09:17 +0530

    Fix: re-added wrapAsync.js

--- utils/wrapasync.js
+++ utils/wrapasync.js
@@ -1,6 +1,9 @@
- module.exports = function wrapAsync(fn) {
+ // Updated to force Git detection
+module.exports = function wrapAsync(fn) {
   return function (req, res, next) {
     fn(req, res, next).catch(next);
   };
 };
+
+
 
